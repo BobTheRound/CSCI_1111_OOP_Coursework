56d2b023f1976671490d901e7ca235a407748181
BobTheRound <134724160+BobTheRound@users.noreply.github.com>
2023-06-26 09:30:25 -0600

    Add files via upload

---
+++ SWTECH_9_01_RECTANGLE_V2/SWTECH_9_01_RECTANGLE/src/rectangleMain.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Robert Beauchamp
+ * Date started: 6/15/2023
+ * Date Completed: 
+ * 
+ * step 1: define height and width parameters
+ * step 2: generate math logic to find perimeter and area.
+ * step 3: ???
+ * step 4: profit
+ */
+public class rectangleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
